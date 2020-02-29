ea050c1501504d26ec9a5959daa004d6208229e5
Collin Riggs <56004264+CollinRiggs@users.noreply.github.com>
2020-02-29 20:14:09 -0330

    test bot

--- bot.js
+++ bot.js
@@ -2,8 +2,9 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = require('./auth.json').token;
 
-bot.login(token);
-
 bot.on('ready', () => {
   console.info('Logged in as ' + bot.user.tag + '!');
+  bot.user.setActivity(" everyone lose SR...", {type: "WATCHING"})
 })
+
+bot.login(token);
